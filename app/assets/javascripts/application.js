82d0a5808601155e42f910b9ab4b8136951d708d
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-07-23 00:12:56 -0500

    hide those cheeses

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'none')
+      e.style.display = 'block';
+   else
+      e.style.display = 'none';
+}
